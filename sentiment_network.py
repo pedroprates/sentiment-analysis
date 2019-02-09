softmax bug fix and parameters changes

sentiment_network.py
@@ -25,7 +25,7 @@ class SentimentNetwork:
         self.polarity_cutoff = polarity_cutoff
         self.pos_neg_ratios = self.calculate_ratios()
 
-        review_vocab, label_vocab = self.pre_process_data(reviews, labels)
+        review_vocab, label_vocab = self.pre_process_data()
         self.review_vocab = list(review_vocab)
         self.label_vocab = list(label_vocab)
 
@@ -48,19 +48,17 @@ class SentimentNetwork:
         self.learning_rate = learning_rate
         self.weights_0_1, self.weights_1_2, self.layer_1 = self.init_network()
 
-    def pre_process_data(self, reviews, labels):
-        """ Processing the data, creating the vocabulary
-
-        :param reviews: Training reviews
-        :param labels: Training labels
+    """ Processing the data, creating the vocabulary
+        
         :return: The review vocabulary, all the words known that will probably impact the prediction and the
                 labels vocabulary.
         """
-        review_counter = Counter([word.lower() for review in reviews for word in review.split(" ")])
-        review_vocab = set({word for word in review_counter.keys() if review_counter[word] > self.min_count})
+    def pre_process_data(self):
+        review_counter = Counter([word.lower() for review in self.reviews for word in review.split(" ")])
+        review_vocab = [word for word in review_counter.keys() if review_counter[word] > self.min_count]
         review_vocab = filter(lambda x: np.absolute(self.pos_neg_ratios[x]) > self.polarity_cutoff, review_vocab)
 
-        label_vocab = set({ label.lower() for label in labels })
+        label_vocab = set({label.lower() for label in self.labels})
 
         return review_vocab, label_vocab
 
@@ -78,9 +76,9 @@ class SentimentNetwork:
         for review, label in zip(self.reviews, self.labels):
             for word in review.split(" "):
                 word = word.lower()
-                if label == 'POSITIVE':
+                if label == 'positive':
                     pos_counter[word] += 1
-                elif label == 'NEGATIVE':
+                elif label == 'negative':
                     neg_counter[word] += 1
 
         # Filtering only words that appear more than min_counts
@@ -125,7 +123,7 @@ class SentimentNetwork:
         :return: sigmoid value
         """
 
-        return 1 / (1 + np.exp(x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def sigmoid_derivative(output):
@@ -142,7 +140,7 @@ class SentimentNetwork:
         """
 
         assert len(self.reviews) == len(self.labels), 'There must be the same amount of training reviews' \
-                                                                  'and training labels.'
+                                                      'and training labels.'
 
         training_reviews = list()
         for review in self.reviews:
@@ -169,7 +167,7 @@ class SentimentNetwork:
             delta_w_0_1 = hidden_error
             delta_w_1_2 = self.layer_1.T * output_error
 
-            self.weights_0_1[review] += self.learning_rate * delta_w_0_1
+            self.weights_0_1[review] += self.learning_rate * delta_w_0_1.T
             self.weights_1_2 += self.learning_rate * delta_w_1_2[:, None]
 
             # Check prediction
@@ -188,6 +186,8 @@ class SentimentNetwork:
 
             if i % 2500 == 0:
                 print('')
+
+        print('')
 
     def run(self, review):
         """ Returns a POSITIVE or NEGATIVE prediction for a given review
@@ -209,7 +209,7 @@ class SentimentNetwork:
         h2 = self.layer_1.dot(self.weights_1_2)
         output = self.sigmoid(h2)
 
-        return 'POSITIVE' if output[0] >= .5 else 'NEGATIVE'
+        return 'positive' if output[0] >= .5 else 'negative'
 
     def test(self, testing_reviews, testing_labels, verbose=True):
         """ Testing function, to run the trained network into a batch of samples and returns its accuracy
@@ -247,7 +247,7 @@ class SentimentNetwork:
         if path[-1] is not '/':
             path += '/'
 
-        print('Saving weights...\n\tWeights_0_1 on ' + path + 'weights_0_1.npy')
+        print('\nSaving weights...\n\tWeights_0_1 on ' + path + 'weights_0_1.npy')
         np.save(os.path.join(path, 'weights_0_1.npy'), self.weights_0_1)
         print('\tWeights_1_2 on ' + path + 'weights_0_1.npy')
         np.save(os.path.join(path, 'weights_1_2.npy'), self.weights_1_2)
@@ -264,7 +264,7 @@ class SentimentNetwork:
         if path[-1] is not '/':
             path += '/'
 
-        print('Loading weights...\n\tWeights_0_1 on ' + path + 'weigths_0_1.npy')
+        print('\nLoading weights...\n\tWeights_0_1 on ' + path + 'weigths_0_1.npy')
         self.weights_0_1 = np.load(os.path.join(path, 'weights_0_1.npy'))
         print('\tWeights_1_2 on ' + path + 'weights_1_2.npy')
         self.weights_1_2 = np.load(os.path.join(path, 'weights_1_2.npy'))